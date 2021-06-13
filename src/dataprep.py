bbab925102a20bb9b2f9d14b2abd8754996a9899
Your Name <you@example.com>
2021-06-13 18:55:14 +0200

    start building the model

--- src/dataprep.py
+++ src/dataprep.py
@@ -2,16 +2,34 @@ import pandas as pd
 import numpy as np
 
 
-def import_data(filepath):
-    all_all_features = ["text_tokens", "hashtags", "tweet_id", "present_media", "present_links", "present_domains",\
-                    "tweet_type","language", "tweet_timestamp", "engaged_with_user_id", "engaged_with_user_follower_count",\
-                "engaged_with_user_following_count", "engaged_with_user_is_verified", "engaged_with_user_account_creation",\
-                "engaging_user_id", "enaging_user_follower_count", "enaging_user_following_count", "enaging_user_is_verified",\
-                "enaging_user_account_creation", "engagee_follows_engager","retweet","reply","like","retweet_with_comment"]
+def encode_hashtags(hashtags):
+    if type(hashtags) == str:
+        return hashtags.split()
+    else:
+        return []
 
-    label_features = ["retweet","reply","like","retweet_with_comment"]
-    
-    ratings_raw = pd.read_csv(filepath, delimiter='\x01',names = all_all_features)
+
+def encode_text_tokens(hashtags):
+    if type(hashtags) == str:
+        return hashtags.split()
+    else:
+        return []
+
+
+def import_data(filepath, limit_dataset=False):
+    all_all_features = ["text_tokens", "hashtags", "tweet_id", "present_media", "present_links", "present_domains",
+                        "tweet_type", "language", "tweet_timestamp", "engaged_with_user_id",
+                        "engaged_with_user_follower_count",
+                        "engaged_with_user_following_count", "engaged_with_user_is_verified",
+                        "engaged_with_user_account_creation",
+                        "engaging_user_id", "enaging_user_follower_count", "enaging_user_following_count",
+                        "enaging_user_is_verified",
+                        "enaging_user_account_creation", "engagee_follows_engager", "retweet", "reply", "like",
+                        "retweet_with_comment"]
+
+    label_features = ["retweet", "reply", "like", "retweet_with_comment"]
+
+    ratings_raw = pd.read_csv(filepath, delimiter='\x01', names=all_all_features)
 
     # set labels to True or False, omit timestamp
     for label in label_features:
@@ -22,5 +40,11 @@ def import_data(filepath):
     ratings_raw["tweet_type"] = ratings_raw["tweet_type"].factorize()[0]
     ratings_raw["present_media"] = ratings_raw["present_media"].factorize()[0]
 
-    return ratings_raw
+    # strings to lists by delimiter
+    ratings_raw["hashtags"] = ratings_raw["hashtags"].apply(encode_hashtags)
+    ratings_raw["text_tokens"] = ratings_raw["text_tokens"].apply(encode_text_tokens)
 
+    if limit_dataset:
+        ratings_raw = ratings_raw[:300]
+
+    return ratings_raw
---
+++ src/model3.py
@@ -0,0 +1,41 @@
+import dataprep
+import pandas as pd
+from sklearn import svm
+
+
+# use: "Text tokens", "Language", "Hashtags"
+
+def get_data(engagement_type):
+    data = dataprep.import_data("../shared/data/project/training/one_hour", limit_dataset=True)
+    data = data.loc[:, ["text_tokens", "hashtags", "language", engagement_type]]
+    return data
+
+
+def get_test_data(engagement_type, number):
+    data = dataprep.import_data("../shared/data/project/training/one_hour")
+    data = data.loc[:, ["text_tokens", "hashtags", "language", engagement_type]]
+    return data.iloc[number]
+
+
+def reply_create_model():
+    data = get_data("reply")
+    features = data.loc[:, ["text_tokens", "hashtags", "language"]]
+    samples = data["reply"]
+
+    regr = svm.SVR()
+    regr.fit(features, samples)
+
+    return regr
+
+
+def reply_pred_model(input_features):
+    model = reply_create_model()
+    prediction = model.predict(input_features)
+    return prediction
+
+
+# test it
+test_tweet = get_test_data("reply", 301)
+print(test_tweet)
+pred = reply_pred_model(test_tweet)
+print("Input: ", test_tweet, "Prediction:", pred)
